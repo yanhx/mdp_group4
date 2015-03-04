0ca7e5e1245e69b0efa6d8551c5558023ad7ee99
freakingchao <fengchao1993@gmail.com>
2015-03-04 11:14:50 +0800

    Changed the UI for control panel

--- map_simulator/ControlPanel.java
+++ map_simulator/ControlPanel.java
@@ -15,67 +15,103 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import map_simulator.Simulator;
+import javax.swing.SwingConstants;
+import java.awt.Color;
 
+@SuppressWarnings("serial")
 public class ControlPanel extends JPanel{
 	public boolean simulatorMode = true;
 	
-	private JButton explorerBtn;
-	private JButton shortestPathBtn;
-	private JLabel mouseLoc, noOfSteps;
-	private JLabel Minutes, String1, Speed1, Explored;
-	private JLabel Seconds, String2;
-	private JButton resetBtn;
-	private JButton LoadFile;
-	private JComboBox combobox, Percentage_combobox;
+	private JPanel locPanel, stepPanel;
+	private JLabel locLbl, noOfStepsLbl;
+	private JLabel locXLbl, locXValueLbl, locYLbl, locYValueLbl, stepLbl, stepValueLbl;
+	private JPanel minutePanel, secondPanel;
+	private JPanel speedPanel, percentPanel;
+	private JPanel string1Panel, string2Panel;
+	
+	private JLabel string1Lbl, string2Lbl;
+	private JLabel minuteLbl, secondLbl, speedLbl, percentLbl;
+	private JComboBox<String> speedCbx, percentCbx;
+	private JButton explorerBtn, shortestPathBtn, resetBtn, loadFileBtn;
+	
 	public String speed;
 	public int speed1, Percentage;
 	public long f_speed;
 	public static int time;
-	private JTextField textField, mapdescriptor1;
-	private JTextField textField_1, mapdescriptor2;
+	private JTextField secondTfd, mapdescriptor1Tfd;
+	private JTextField minuteTfd, mapdescriptor2Tfd;
 	private static String file ="map layout/map.txt";
 	static int interval;
 	static Timer timer;
+	
 	BoardPanel board;
 	public ControlPanel(BoardPanel board){
 		this.board = board;
 		setLayout(new GridLayout(0,1));
 	
 		//JPanel mapFilePanel = new JPanel();
-		JPanel buttonsPanel = new JPanel();
-		Minutes = new JLabel("Minutes:");
-		Seconds = new JLabel("Seconds:");
-		String1 = new JLabel("String 1:");
-		String2 = new JLabel("String 2:");
-		Speed1 = new JLabel("Speed:");
-		Explored = new JLabel("Exploration %");
-		textField = new JTextField();
-		textField.setText("0");
-		textField_1 = new JTextField();
-		textField_1.setText("6");
-		mapdescriptor1 = new JTextField();
-		mapdescriptor2 = new JTextField();
 		
-		//X and Y label
-		mouseLoc = new JLabel("X:            Y: ");
-		noOfSteps = new JLabel("Steps: 0");
+		JPanel buttonsPanel = new JPanel();
+		buttonsPanel.setBackground(new Color(135, 206, 235));
+		
+		locPanel = new JPanel();
+		stepPanel = new JPanel();
+		minutePanel = new JPanel();
+		secondPanel = new JPanel();
+		speedPanel = new JPanel();
+		percentPanel = new JPanel();
+		string1Panel = new JPanel();
+		string2Panel = new JPanel();
+		
+		locXLbl = new JLabel("X:");
+		locXLbl.setHorizontalAlignment(SwingConstants.CENTER);
+		locXValueLbl = new JLabel(""); 
+		locXValueLbl.setHorizontalAlignment(SwingConstants.CENTER);
+		locYLbl = new JLabel("Y:");
+		locYLbl.setHorizontalAlignment(SwingConstants.CENTER);
+		locYValueLbl = new JLabel("");
+		locYValueLbl.setHorizontalAlignment(SwingConstants.CENTER);
+		stepLbl = new JLabel("Steps:");
+		stepLbl.setHorizontalAlignment(SwingConstants.CENTER);
+		stepValueLbl = new JLabel("");
+		stepValueLbl.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		minuteLbl = new JLabel("Minutes:");
+		minuteLbl.setHorizontalAlignment(SwingConstants.CENTER);
+		secondLbl = new JLabel("Seconds:");
+		secondLbl.setHorizontalAlignment(SwingConstants.CENTER);
+		string1Lbl = new JLabel("String 1:");
+		string1Lbl.setHorizontalAlignment(SwingConstants.CENTER);
+		string2Lbl = new JLabel("String 2:");
+		string2Lbl.setHorizontalAlignment(SwingConstants.CENTER);
+		speedLbl = new JLabel("Speed:");
+		speedLbl.setHorizontalAlignment(SwingConstants.CENTER);
+		percentLbl = new JLabel("Complete %");
+		percentLbl.setHorizontalAlignment(SwingConstants.CENTER);
+		secondTfd = new JTextField();
+		secondTfd.setText("0");
+		minuteTfd = new JTextField();
+		minuteTfd.setText("6");
+		mapdescriptor1Tfd = new JTextField();
+		mapdescriptor2Tfd = new JTextField();
 		
 		//Percentage button
-			Percentage_combobox = new JComboBox();
-			Percentage_combobox.setPreferredSize(new Dimension(200,50));
+			percentCbx = new JComboBox();
+			percentCbx.setBackground(new Color(135, 206, 250));
+			percentCbx.setPreferredSize(new Dimension(200,50));
    		//combobox.addItem("--Select Speed--");
-			Percentage_combobox.addItem("100");
-			Percentage_combobox.addItem("90");
-	   		Percentage_combobox.addItem("80");
-	   		Percentage_combobox.addItem("70");
-	   		Percentage_combobox.addItem("60");
-	   		Percentage_combobox.setSelectedIndex(0);
+			percentCbx.addItem("100");
+			percentCbx.addItem("90");
+	   		percentCbx.addItem("80");
+	   		percentCbx.addItem("70");
+	   		percentCbx.addItem("60");
+	   		percentCbx.setSelectedIndex(0);
               getPercentage(100);
 	
-             Percentage_combobox.addActionListener(new ActionListener() {
+             percentCbx.addActionListener(new ActionListener() {
 		@Override
 		public void actionPerformed(ActionEvent e) {
-		       String Percent_Explored = (String) Percentage_combobox.getSelectedItem();
+		       String Percent_Explored = (String) percentCbx.getSelectedItem();
 	           int Percent_Explored1 = Integer.parseInt(Percent_Explored);
 	           getPercentage(Percent_Explored1);
 	           System.out.println(Percent_Explored1);
@@ -84,9 +120,9 @@ public class ControlPanel extends JPanel{
 	});
 		
 		//loadfile
-		LoadFile = new JButton("Load File");
-		LoadFile.setPreferredSize(new Dimension(200,50));
-		LoadFile.addActionListener(new ActionListener(){
+		loadFileBtn = new JButton("Load File");
+		loadFileBtn.setPreferredSize(new Dimension(200,50));
+		loadFileBtn.addActionListener(new ActionListener(){
 
 		 final JFileChooser  fileDialog = new JFileChooser();
 			public void actionPerformed(ActionEvent arg0) {
@@ -105,22 +141,22 @@ public class ControlPanel extends JPanel{
 		      });
 					
 		//choose map file panel
-		//combobox 
-			combobox = new JComboBox();
-		    combobox.setPreferredSize(new Dimension(200,50));
+			speedCbx = new JComboBox();
+			speedCbx.setBackground(new Color(135, 206, 250));
+		    speedCbx.setPreferredSize(new Dimension(200,50));
 	   		//combobox.addItem("--Select Speed--");
-		   		  combobox.addItem("50");
-		   		  combobox.addItem("100");
-	              combobox.addItem("200");
-	              combobox.addItem("300");
-	              combobox.addItem("1000");
-	              combobox.setSelectedIndex(0);
+		   		  speedCbx.addItem("50");
+		   		  speedCbx.addItem("100");
+	              speedCbx.addItem("200");
+	              speedCbx.addItem("300");
+	              speedCbx.addItem("1000");
+	              speedCbx.setSelectedIndex(0);
 	              setspeed(50);
 		
-	        combobox.addActionListener(new ActionListener() {
+	        speedCbx.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-		           speed = (String) combobox.getSelectedItem();
+		           speed = (String) speedCbx.getSelectedItem();
 		           speed1 = Integer.parseInt(speed);
 		           setspeed(speed1);
 		           
@@ -137,8 +173,8 @@ public class ControlPanel extends JPanel{
 			@Override
 			public void actionPerformed(ActionEvent e){
 				board.setMouseClick(false);
-				String data = textField_1.getText(); 
-			    String data1 = textField.getText();
+				String data = minuteTfd.getText(); 
+			    String data1 = secondTfd.getText();
 			    int seconds = Integer.parseInt(data1);
 			    int minutes = Integer.parseInt(data);
 			    time = seconds + minutes*60;
@@ -151,11 +187,12 @@ public class ControlPanel extends JPanel{
 
 			        public void run() {
 			            setInterval();
-			            textField.setText(""+interval%60);
-		                textField_1.setText(""+interval/60);
+			            secondTfd.setText(""+interval%60);
+		                minuteTfd.setText(""+interval/60);
 			        }
 			    }, 0, 1000);
-			    explorerBtn.setVisible(false);
+			    explorerBtn.setEnabled(false);
+//			    explorerBtn.setVisible(false);
 			}
 		});
 		
@@ -186,55 +223,77 @@ public class ControlPanel extends JPanel{
 					timer.cancel();
 				}
 				explorerBtn.setVisible(true);
-				textField.setText(""+time%60);
-				textField_1.setText(""+time/60);
+				secondTfd.setText(""+time%60);
+				minuteTfd.setText(""+time/60);
 			}
 		});
 		
+		locPanel.setLayout(new GridLayout(0, 4, 0, 0));
+		locPanel.add(locXLbl);
+		locPanel.add(locXValueLbl);
+		locPanel.add(locYLbl);
+		locPanel.add(locYValueLbl);
 		
-				
-		buttonsPanel.setLayout(new GridLayout(9,4));
-		buttonsPanel.add(mouseLoc);
-		buttonsPanel.add(noOfSteps);
-		buttonsPanel.add(String1);
-		buttonsPanel.add(mapdescriptor1);
-		buttonsPanel.add(String2);
-		buttonsPanel.add(mapdescriptor2);
-		buttonsPanel.add(Minutes);
-		buttonsPanel.add(textField_1);
-		buttonsPanel.add(Seconds);
-		buttonsPanel.add(textField);
-		buttonsPanel.add(Speed1);
-		buttonsPanel.add(combobox);
-		buttonsPanel.add(Explored);
-		buttonsPanel.add(Percentage_combobox);
+		stepPanel.setLayout(new GridLayout(0, 4, 0, 0));
+		stepPanel.add(stepLbl);
+		stepPanel.add(stepValueLbl);
+		
+		minutePanel.setLayout(new GridLayout(2, 0, 0, 0));
+		minutePanel.add(minuteLbl);
+		minutePanel.add(minuteTfd);
+		
+		secondPanel.setLayout(new GridLayout(2, 0, 0, 0));
+		secondPanel.add(secondLbl);
+		secondPanel.add(secondTfd);
+		
+		string1Panel.setLayout(new GridLayout(2, 0, 0, 0));
+		string1Panel.add(string1Lbl);
+		string1Panel.add(mapdescriptor1Tfd);
+		
+		string2Panel.setLayout(new GridLayout(2, 0, 0, 0));
+		string2Panel.add(string2Lbl);
+		string2Panel.add(mapdescriptor2Tfd);
+		
+		speedPanel.setLayout(new GridLayout(2, 0, 0, 0));
+		speedPanel.add(speedLbl);
+		speedPanel.add(speedCbx);
+
+		percentPanel.setLayout(new GridLayout(2, 0, 0, 0));
+		percentPanel.add(percentLbl);
+		percentPanel.add(percentCbx);
+		
+		buttonsPanel.setLayout(new GridLayout(0, 2, 0, 0));
+		buttonsPanel.add(locPanel);
+		buttonsPanel.add(stepPanel);
+		buttonsPanel.add(string1Panel);
+		buttonsPanel.add(string2Panel);
+		buttonsPanel.add(minutePanel);
+		buttonsPanel.add(secondPanel);
+		buttonsPanel.add(speedPanel);
+		buttonsPanel.add(percentPanel);
 		buttonsPanel.add(explorerBtn);
 		buttonsPanel.add(shortestPathBtn);
-		buttonsPanel.add(LoadFile);
+		buttonsPanel.add(loadFileBtn);
 		buttonsPanel.add(resetBtn);
 
-
-		
-		
-		//add all panels into control panel
-		//add(mapFilePanel);
 		add(buttonsPanel);
 	}
 	
 	public void setString1 (String string1) {
-		mapdescriptor1.setText(string1);
+		mapdescriptor1Tfd.setText(string1);
 	}
 	
 	public void setString2 (String string2) {
-		mapdescriptor2.setText(string2);
+		mapdescriptor2Tfd.setText(string2);
 	}
 	
 	public void setMouseLoc (int x, int y) {
-		mouseLoc.setText("X: " + Integer.toString(x) + "        Y: " + Integer.toString(y));
+		locXValueLbl.setText(Integer.toString(x));
+		locYValueLbl.setText(Integer.toString(y));
 	}
 	
 	public void setNoOfSteps (int steps) {
-		noOfSteps.setText("Steps: " + Integer.toString(steps));
+		stepValueLbl.setText(Integer.toString(steps));
 	}
 	
 	public void getPercentage(int Percentage) {
